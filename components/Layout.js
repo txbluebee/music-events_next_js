8d0604dc51a874d605abc854c6956dc904712392
Brian Fan <kuomu.fan@gmail.com>
2021-05-13 15:57:49 -0700

    add 404 error page

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
-import styles from '../styles/Layout.module.css';
+import Showcase from './Showcase';
+import styles from '@/styles/Layout.module.css';
+
+import { useRouter } from 'next/router';
 
 export default function Layout({ title, description, keywords, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -12,6 +17,7 @@ export default function Layout({ title, description, keywords, children }) {
         <meta name='keywords' content={keywords} />
       </Head>
       <Header />
+      {router.pathname === '/' && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
