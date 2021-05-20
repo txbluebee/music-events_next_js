b53942d749a3ca7b61bbcfa437550598f34c0130
Brian Fan <kuomu.fan@gmail.com>
2021-05-20 08:33:57 -0700

    add login and register page

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,13 @@
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useContext } from 'react';
 import Link from 'next/link';
 import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.css';
 
 export default function Header() {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -14,11 +19,38 @@ export default function Header() {
           <li>
             <Link href='/events'>Events</Link>
           </li>
-          <li>
-            <Link href='/events/add'>
-              <a>Add Event</a>
-            </Link>
-          </li>
+          {user ? (
+            // if logged in
+            <>
+              <li>
+                <Link href='/events/add'>
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/account/dashboard'>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className='btn-secondary btn-icon'
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // if logged out
+            <li>
+              <Link href='/account/login'>
+                <a className='btn-secondary btn-icon'>
+                  <FaSignInAlt /> Login
+                </a>
+              </Link>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
