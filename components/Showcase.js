b97c3c7f92e3371b849f47d857426302bc41d1a7
Brian Fan <kuomu.fan@gmail.com>
2021-05-22 10:40:37 -0700

    remove showcase

--- components/Showcase.js
+++ components/Showcase.js
@@ -1,8 +1,6 @@
-import styles from '../styles/Showcase.module.css';
-
 export default function Showcase() {
   return (
-    <div className={styles.showcase}>
+    <div>
       <h1>Welcome To The Party!</h1>
       <h2>Find the hottest DJ events</h2>
     </div>
